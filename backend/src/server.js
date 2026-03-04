50b986674028cec3f701a60d63e841b946b3c7fd
Sai Narendra <tannirusainarendra81@gmail.com>
2026-03-04 20:35:43 +0530

    update

--- backend/src/server.js
+++ backend/src/server.js
@@ -5,7 +5,7 @@ const dotenv = require("dotenv");
 dotenv.config();
 connectDB();
 
-const PORT = 5000;
+const PORT = 8000;
 app.listen(PORT, () => {
   console.log(`🚀 Server running on port http://localhost:${PORT}`);
 });
